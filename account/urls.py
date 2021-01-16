83dd13641d71e8b9e80f5c4e9a2069e6ea8f0c68
asvitaliii <a.s.vitaliii@gmail.com>
2021-01-16 11:12:37 +0200

    making site

--- account/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import register, sign_in
-
-urlpatterns = [
-    path('register/', register, name='register'),
-    path('sign_in/', sign_in, name='sign_in'),
-]
